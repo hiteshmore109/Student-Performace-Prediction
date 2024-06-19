eec96e4f024459495b1db93c600195d848024dd0
hiteshmore109 <hitesh.omprakashmore@gmail.com>
2024-06-19 13:59:02 +0530

    Base Project setup

---
+++ README.md
@@ -0,0 +1,3 @@
+## This is my first Machine learning End-To-End project where I am going to deploy the project in the end
+
+*Currently the project is in development*
---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+from typing import List
+
+
+def get_requirements(filepath:str)->List[str]:
+    """
+    Reads a file containing a list of requirements and returns them as a list of strings.
+
+    Args:
+        filepath (str): The path to the file containing the requirements.
+
+    Returns:
+        List[str]: A list of strings representing the requirements.
+    """
+    with open(file=filepath) as file:
+        requirements = file.readlines()
+        requirements = [requirement.replace("\n", "") for requirement in requirements]
+
+        if "-e ." in requirements:
+            requirements.remove("-e .")
+
+        return requirements
+    
+
+setup(
+    name="student-performance-prediction",
+    version="0.0.1",
+    author="Hitesh More",
+    author_email="hitesh.omprakashmore@gmail.com",
+    packages = find_packages(),
+    install_requires=get_requirements("requirements.txt")
+)
